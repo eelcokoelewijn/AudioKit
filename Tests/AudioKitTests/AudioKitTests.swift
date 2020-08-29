0ba1a6ae4b1181d0b2411d7028ae3eebdc98e32d
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2020-08-29 21:38:45 +0200

    Update swift-tools version

--- Tests/AudioKitTests/AudioKitTests.swift
+++ Tests/AudioKitTests/AudioKitTests.swift
@@ -6,7 +6,7 @@ final class AudioKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AudioKit().text, "Hello, World!")
+        XCTAssertEqual(PlayerController().isPlaying, false)
     }
 
     static var allTests = [
